b35589b046303e0b6a0ad5be12fd67d35eb82276
matrickpf21 <patrickfigueroa23@gmail.com>
2024-03-14 11:41:57 -0500

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
+
+module.exports = config;
