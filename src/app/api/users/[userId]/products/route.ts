8e5f28b95b1c7c731c97d8c48515fef9ab6e2807
matrickpf21 <patrickfigueroa23@gmail.com>
2024-03-16 14:59:34 -0500

    feat: add GET methods for users

--- src/app/api/users/[userId]/products/route.ts
+++ src/app/api/users/[userId]/products/route.ts
@@ -6,88 +6,131 @@ import { db } from "@/server/db";
 import type { ZodInferSchema } from "@/types";
 
 const createSchema = z.object<
-  ZodInferSchema<
-    Omit<
-      Product,
-      "id" | "createdAt" | "updatedAt" | "status" | "image" | "userId"
-    > & {
-      // image: Blob;
-    }
-  >
+	ZodInferSchema<
+		Omit<
+			Product,
+			"id" | "createdAt" | "updatedAt" | "status" | "image" | "userId"
+		> & {
+			// image: Blob;
+		}
+	>
 >({
-  costInCents: z.number({ coerce: true }),
-  description: z
-    .string()
-    .transform((f) => (f === "null" ? null : f))
-    .nullable(),
-  // image: z.instanceof(Blob),
-  name: z.string(),
+	costInCents: z.number({ coerce: true }),
+	description: z
+		.string()
+		.transform(f => (f === "null" ? null : f))
+		.nullable(),
+	// image: z.instanceof(Blob),
+	name: z.string(),
 });
 
 async function createProduct(
-  req: NextRequest,
-  ctx: { params: { userId: string } },
+	req: NextRequest,
+	ctx: { params: { userId: string } },
 ) {
-  try {
-    const { params } = ctx;
+	try {
+		const { params } = ctx;
 
-    const body = await req.json();
-    const parse = createSchema.safeParse(body);
+		const body = await req.json();
+		const parse = createSchema.safeParse(body);
 
-    if (!parse.success) {
-      console.error(parse.error.issues);
+		if (!parse.success) {
+			console.error(parse.error.issues);
 
-      return NextResponse.json(
-        {
-          message: "Peticion invalida",
-          errors: parse.error.issues,
-        },
-        { status: 400 },
-      );
-    }
+			return NextResponse.json(
+				{
+					message: "Peticion invalida",
+					errors: parse.error.issues,
+				},
+				{ status: 400 },
+			);
+		}
 
-    const user = await db.user.findUnique({
-      where: { id: params.userId },
-    });
+		const user = await db.user.findUnique({
+			where: { id: params.userId },
+		});
 
-    if (!user) {
-      return NextResponse.json(
-        {
-          message: "El usuario no existe",
-        },
-        { status: 400 },
-      );
-    }
+		if (!user) {
+			return NextResponse.json(
+				{
+					message: "El usuario no existe",
+				},
+				{ status: 400 },
+			);
+		}
 
-    const product = await db.product.create({
-      data: {
-        ...parse.data,
-        image: null,
-        user: {
-          connect: {
-            id: params.userId,
-          },
-        },
-      },
-    });
+		const product = await db.product.create({
+			data: {
+				...parse.data,
+				image: null,
+				user: {
+					connect: {
+						id: params.userId,
+					},
+				},
+			},
+		});
 
-    return NextResponse.json(
-      {
-        message: "Solicitud exitosa",
-        product,
-      },
-      { status: 201 },
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      {
-        message: error.message,
-      },
-      {
-        status: 500,
-      },
-    );
-  }
+		return NextResponse.json(
+			{
+				message: "Solicitud exitosa",
+			},
+			{ status: 201 },
+		);
+	} catch (error: any) {
+		return NextResponse.json(
+			{
+				message: error.message,
+			},
+			{
+				status: 500,
+			},
+		);
+	}
 }
 
-export { createProduct as POST };
+async function getUserByIdWithProducts(
+	req: NextRequest,
+	ctx: { params: { userId: string } },
+) {
+	try {
+		const { params } = ctx;
+
+		const user = await db.user.findUnique({
+			where: { id: params.userId },
+			include: {
+				products: {
+					where: {
+						status: true,
+					},
+				},
+			},
+		});
+
+		if (!user) {
+			return NextResponse.json(
+				{
+					message: "El usuario no existe",
+				},
+				{ status: 400 },
+			);
+		}
+
+		return NextResponse.json(
+			{
+				data: user,
+				message: "Solicitud exitosa",
+			},
+			{ status: 200 },
+		);
+	} catch (error: any) {
+		return NextResponse.json(
+			{
+				message: error.message,
+			},
+			{ status: 500 },
+		);
+	}
+}
+
+export { createProduct as POST, getUserByIdWithProducts as GET };
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -6,82 +6,103 @@ import { db } from "@/server/db";
 import type { ZodInferSchema } from "@/types";
 
 const createSchema = z.object<
-  ZodInferSchema<
-    Omit<User, "id" | "createdAt" | "updatedAt" | "emailVerified" | "image">
-  >
+	ZodInferSchema<
+		Omit<User, "id" | "createdAt" | "updatedAt" | "emailVerified" | "image">
+	>
 >({
-  type: z.nativeEnum(UserType),
-  email: z
-    .string()
-    .email()
-    .transform((v) => (v === "null" ? null : v))
-    .nullable(),
-  name: z
-    .string()
-    .transform((v) => (v === "null" ? null : v))
-    .nullable(),
-  nameHandler: z
-    .string()
-    .regex(/^[a-zA-Z0-9_]*$/)
-    .transform((v) => (v === "null" ? null : v))
-    .nullable(),
-  description: z
-    .string()
-    .transform((v) => (v === "null" ? null : v))
-    .nullable(),
-  direction: z
-    .string()
-    .transform((v) => (v === "null" ? null : v))
-    .nullable(),
-  phone: z
-    .string()
-    .transform((v) => (v === "null" ? null : v))
-    .nullable(),
+	type: z.nativeEnum(UserType),
+	email: z
+		.string()
+		.email()
+		.transform(v => (v === "null" ? null : v))
+		.nullable(),
+	name: z
+		.string()
+		.transform(v => (v === "null" ? null : v))
+		.nullable(),
+	nameHandler: z
+		.string()
+		.regex(
+			/^[a-zA-Z0-9_]*$/,
+			"Solo se permiten letras, numeros y guiones bajos",
+		)
+		.transform(v => (v === "null" ? null : v))
+		.nullable(),
+	description: z
+		.string()
+		.transform(v => (v === "null" ? null : v))
+		.nullable(),
+	direction: z
+		.string()
+		.transform(v => (v === "null" ? null : v))
+		.nullable(),
+	phone: z
+		.string()
+		.transform(v => (v === "null" ? null : v))
+		.nullable(),
 });
 
 async function createUser(req: NextRequest) {
-  try {
-    const formData = await req.formData();
+	try {
+		const formData = await req.formData();
 
-    const body = Object.fromEntries(formData.entries());
+		const body = Object.fromEntries(formData.entries());
 
-    const parse = createSchema.safeParse(body);
+		const parse = createSchema.safeParse(body);
 
-    if (!parse.success) {
-      console.error(parse.error.issues);
+		if (!parse.success) {
+			console.error(parse.error.issues);
 
-      return NextResponse.json(
-        {
-          message: "Peticion invalida",
-          errors: parse.error.issues,
-        },
-        { status: 400 },
-      );
-    }
+			return NextResponse.json(
+				{
+					message: "Peticion invalida",
+					errors: parse.error.issues,
+				},
+				{ status: 400 },
+			);
+		}
 
-    const { data } = parse;
+		const { data } = parse;
 
-    const user = await db.user.create({
-      data,
-    });
+		const user = await db.user.create({
+			data,
+		});
 
-    console.log({ user });
+		console.log({ user });
 
-    return NextResponse.json(
-      {
-        message: "Solicitud exitosa",
-      },
-      { status: 201 },
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      { message: error.message },
-      {
-        status: 500,
-      },
-    );
-  }
+		return NextResponse.json(
+			{
+				message: "Solicitud exitosa",
+			},
+			{ status: 201 },
+		);
+	} catch (error: any) {
+		return NextResponse.json(
+			{ message: error.message },
+			{
+				status: 500,
+			},
+		);
+	}
 }
 
-export { createUser as POST };
+async function getUsers(req: NextRequest) {
+	try {
+		const users = await db.user.findMany({
+			orderBy: {
+				createdAt: "desc",
+			},
+		});
 
+		return NextResponse.json({ data: users, message: "Solicitud exitosa" });
+	} catch (error: any) {
+		return NextResponse.json(
+			{
+				message: error.message,
+			},
+			{ status: 500 },
+		);
+	}
+}
+
+export { getUsers as GET, createUser as POST };
