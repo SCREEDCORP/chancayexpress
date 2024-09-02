f4c2b94b20d459d28a80c4c453b2742b0ad6db10
matrickpf21 <patrickfigueroa23@gmail.com>
2024-09-02 16:27:01 -0500

    setup react-query-kit config

--- src/core/react-query.ts
+++ src/core/react-query.ts
@@ -1,4 +1,32 @@
 import type { QueryClientConfig } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import type { Middleware, MutationHook } from "react-query-kit";
+
+export const mutationMiddleware: Middleware<MutationHook> = useMutationNext => {
+	return (options, queryClient) => {
+		const router = useRouter();
+
+		return useMutationNext(
+			{
+				...options,
+				onSuccess: async (response, variables, context) => {
+					console.log({ response });
+
+					options.onSuccess?.(response, variables, context);
+
+					const mutationKey = options.mutationKey;
+					await queryClient?.invalidateQueries({
+						queryKey: mutationKey,
+					});
+					queryClient?.removeQueries({ queryKey: mutationKey });
+
+					router.refresh();
+				},
+			},
+			queryClient,
+		);
+	};
+};
 
 export const reactQueryConfig = {
 	defaultOptions: {
@@ -8,7 +36,7 @@ export const reactQueryConfig = {
 		mutations: {
 			// react-query-kit use this field for middleware purposes
 			// cannot redeclare with declare module
-			// use: [mutationMiddleware],
+			use: [mutationMiddleware],
 		},
 	},
 } as QueryClientConfig;
